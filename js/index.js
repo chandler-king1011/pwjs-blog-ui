1e0a1bbd5ec7f12f9b6207467dae438d896343a3
Chandler King <ckmotox356@gmail.com>
2020-03-15 16:30:39 -0600

    Completed the index.js file to fetch the post data from an API and inject the correct HTML.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-
-
 const API_URL = "http://localhost:3000/api/posts";
 const API_BASE_URL = "http://localhost:3000/";
 
@@ -8,8 +6,28 @@ window.onload = () => {
 }
 
 const getPosts = () => {
+    fetch(API_URL, {
+        method: "GET"
+    }).then(response => {
+        return response.json();
+    }).then(data=> {
+        buildPosts(data);
+    }).catch(error => {
+        console.log(error);
+    })
 }
 
 const buildPosts = (blogPosts) => {
-
+    document.querySelector(".main_container").innerHTML = blogPosts.map(post => {
+        const postDate = new Date(parseInt(post.added_date)).toDateString();
+        const imageURL = `${API_BASE_URL}${post.post_image}`;
+        return `<a class="post">
+        <div class="post_image" style="background-image: url(${imageURL})"></div>
+        <div class="post_content">
+            <div class="post_date">${postDate}</div>
+            <div class="post_title">${post.title}</div>
+            <div class="post_text">${post.content}</div>
+        </div>
+    </a>`
+    }).join(" ");
 }
