707f46a6650ead03f35e3bc3e1a220173e294493
Chandler King <ckmotox356@gmail.com>
2020-03-16 18:32:27 -0600

    Finshed the add post page and functionality.

--- js/new-post.js
+++ js/new-post.js
@@ -1,5 +1,30 @@
 const API_URL = "http://localhost:3000/api/posts";
+const error = document.querySelector(".error");
+
+
 
 const submitNewPost = () => {
+    let files = document.getElementById("form-post-image").files;
+    if(files.length < 1) {
+        error.innerText = "An image must be selected."
+    } else {
+    let blogPost = new FormData();
+        blogPost.append("title", document.querySelector("#form-post-title").value);
+        blogPost.append("content", document.querySelector("#form-post-content").value);
+        blogPost.append("post-image", files[0]);
 
+        fetch(API_URL, {
+            method: "POST",
+            body: blogPost
+        }).then(() => {
+            window.location.href = "./index.html";
+        })
+    }
 }
+
+const clearError = () => {
+    error.innerText = "";
+}
+
+document.getElementById("form-post-image").addEventListener("change", clearError);
+
